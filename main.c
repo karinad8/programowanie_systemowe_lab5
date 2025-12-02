3aa494190768370eee29d8497d04933866df5dc0
karinad8 <154664496+karinad8@users.noreply.github.com>
2025-12-02 20:35:31 +0100

    Refactor main function to use server and worker loops

--- main.c
+++ main.c
@@ -2,41 +2,30 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include "server.h"
+#include "worker.h"
 
 int main() {
         char str[50];
-  int fd[2];
-  char str2[50];
-  int fd2[2];
+        int fd[2];
+        char str2[50];
+        int fd2[2];
 
-  pipe(fd);
-  pipe(fd2);
-  printf("Wpisz tekst: ");
-  scanf("%s", str);
+        pipe(fd);
+        pipe(fd2);
 
-  pid_t pid = fork();
+        pid_t pid = fork();
 
-  if (pid == 0)
-  {
-    close(fd[1]);
-    close(fd2[0]);
-    read(fd[0], str2, sizeof(str2));
-    close(fd[0]);
-    str2[0] = 'X';
-    write(fd2[1], str2, strlen(str2) + 1);
-    close(fd2[1]);
-  }
-  else
-  {
-    close(fd[0]);
-    close(fd2[1]);
-    write(fd[1], str, strlen(str) + 1);
-    close(fd[1]);
-
-    read(fd2[0], str2, sizeof(str2));
-    close(fd2[0]);
-    printf("Od dziecka: %s\n", str2);
-  }
-
-  return 0;
+        if (pid == 0) {
+                close(fd[1]);
+                close(fd2[0]);
+                worker_loop(fd[0], fd2[1]);
+        }
+        else {
+                close(fd[0]);
+                close(fd2[1]);
+                server_loop(fd[1], fd2[0]);
+                wait(NULL);
+        }
+        return 0;
 }
